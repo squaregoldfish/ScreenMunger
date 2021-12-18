d083aec7618dc8d9ffe547ee31976178826d6952
Steve Jones <steve@squaregoldfish.co.uk>
2021-12-18 15:25:23 +0100

    Delete old files

--- daily_munger.sh
+++ daily_munger.sh
@@ -4,3 +4,5 @@ yesterday=`date -d 'yesterday' +%Y%m%d`
 yesterday_long=`date -d 'yesterday' +%Y-%m-%d`
 
 pipenv run python daily_pixels.py ./uploads/$yesterday $yesterday_long "Image constructed by taking one screenshot per minute; each column of pixels is the average pixel colour of the screenshot."
+
+rm -r ./uploads/$yesterday
