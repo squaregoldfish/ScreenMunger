5b4fb0eca304c74ce8a51d6b8a757c190ade416f
Steve Jones <steve@squaregoldfish.co.uk>
2022-06-11 10:18:30 +0200

    Disabled, but still cleans up files

--- daily_munger.sh
+++ daily_munger.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-yesterday=`date -d 'yesterday' +%Y%m%d`
+#yesterday=`date -d 'yesterday' +%Y%m%d`
 day_before_yesterday=`date -d '-2 day' +%Y%m%d`
 
-yesterday_long=`date -d 'yesterday' +%Y-%m-%d`
+#yesterday_long=`date -d 'yesterday' +%Y-%m-%d`
 
-pipenv run python daily_pixels.py ./uploads/$yesterday $yesterday_long "Image constructed by taking one screenshot per minute; each column of pixels is the average pixel colour of the screenshot."
+#pipenv run python daily_pixels.py ./uploads/$yesterday $yesterday_long "Image constructed by taking one screenshot per minute; each column of pixels is the average pixel colour of the screenshot."
 
 rm -r ./uploads/$day_before_yesterday
 
