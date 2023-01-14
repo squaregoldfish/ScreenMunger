79a198b887a620ec41489e9454d093cb8b14b873
Steve Jones <steve@squaregoldfish.co.uk>
2023-01-14 21:53:25 +0100

    Improved video alt text

--- hourly_sorter.sh
+++ hourly_sorter.sh
@@ -78,7 +78,7 @@ then
       vidfile=`ls -t uploads/video |tail -1`
       if [ -n "$vidfile" ]
       then
-        pipenv run python postimage.py "uploads/video/$vidfile" "Random frame from a video sorted by random criteria"
+        pipenv run python postimage.py "uploads/video/$vidfile" "Random frame from a video sorted by random criteria, or stripes coloured by each frame of a video"
         rm "uploads/video/$vidfile"
       fi
     fi
