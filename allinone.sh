30e46ec773ad2b68042fb34edd6557d450cc978e
Steve Jones <steve@squaregoldfish.co.uk>
2024-01-24 20:01:56 +0100

    Improved all-in-one script

--- allinone.sh
+++ allinone.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-TMPDIR='/tmp'
+yt-dlp --cookies-from-browser firefox --retries infinite -S "height:720" -o tmp_vid $1
+pipenv run python frame_munger.py tmp_vid.* && rm tmp_vid.*
 
-metadata=`yt-dlp -j $1`
-
-title=`echo $metadata | sed 's/.*"title": "\(.*\)", "formats".*/\1/g'`
-filename="${TMPDIR}/${title}.mp4"
-
-if `echo $metadata | grep -q "22 - "`
-then
-  yt-dlp -f "best[height=720]" --recode-video mp4 -o "$filename" $1
-else
-  yt-dlp --recode-video mp4 -o "$filename" $1
-fi
-
-pipenv run python frame_munger.py "$filename" && rm "$filename"
