ec90ccae16fcd381fde8b457c410c76f28f2668b
Steve Jones <steve@squaregoldfish.co.uk>
2024-06-15 21:09:53 +0200

    Do not capture if screensaver is running

--- capture.sh
+++ capture.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
+do_capture=1
 
-dir="`dirname $0`/uploads/`date '+%Y%m%d'`"
+# See if xscreensaver is installed.
+# If so, and if the screensaver is running,
+# we don't capture.
 
-if [ ! -d $dir ]
+if command -v xscreensaver-command &> /dev/null
 then
-  mkdir -p $dir
+    status=`xscreensaver-command -time`
+    case "$status" in
+        *non-blanked*)
+            do_capture=1
+            ;;
+        *)
+            do_capture=0
+    esac
+else
+    do_capture=1
 fi
 
-DISPLAY=:0 scrot $dir/`date '+%Y%m%d%H%M'`00.png
+
+if [ $do_capture -eq 1 ]
+then
+    dir="`dirname $0`/uploads/`date '+%Y%m%d'`"
+
+    if [ ! -d $dir ]
+    then
+        mkdir -p $dir
+    fi
+
+    DISPLAY=:0 scrot $dir/`date '+%Y%m%d%H%M'`00.png
+fi
 
